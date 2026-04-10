45b9ce853ef1e72b02ed5c7d469f2bf3696b2d69
Bagus Aditama <bagusaditama24@example.com>
2026-04-10 09:55:26 +0700

    Update Firebase config with new project details

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ export const mqttConfig = {
 
 export const firebaseConfig = {
     apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "counter-iot-phi.firebaseapp.com",
-    databaseURL: "https://counter-iot-phi-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "counter-iot-phi",
-    appId: "YOUR_FIREBASE_APP_ID"
+    authDomain: "phi-3764f.firebaseapp.com",
+    databaseURL: "https://phi-3764f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "phi-3764f",
+    appId: "1:904957731712:web:e5f21e2e377dff31c49765"
 };
